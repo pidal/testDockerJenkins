2ec065414fca330ef1f4127ba00ba685e5e524f3
Alfonso Pidal <alfonsopidal@gmail.com>
2019-04-02 20:13:20 +0200

    Initial commit

---
+++ src/main/java/com/ufv/parcial/dockertest/App.java
@@ -0,0 +1,13 @@
+package com.ufv.parcial.dockertest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
